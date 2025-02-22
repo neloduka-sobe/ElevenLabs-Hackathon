ed4438195244b682eb418705693e4618939524d4
Cezary24 <cezarypastor555@gmail.com>
2025-02-22 22:10:33 +0100

    Routing

--- frontend/data-hub/src/components/toolbar/toolbar.component.ts
+++ frontend/data-hub/src/components/toolbar/toolbar.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatIconModule,MatButtonModule,MatToolbarModule],
+  imports: [MatIconModule,MatButtonModule,MatToolbarModule,MatMenuModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
 
+constructor(private router: Router){}
+
+  onUpload(): void {
+    this.router.navigate(['/upload']);
+  }
+
+  onFind(): void {
+    this.router.navigate(['/search']);
+  }
 }
